2d892e084331a1ec66a0fa87769bdfa981f418e5
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-01-13 13:54:05 +0100

    remove unwanted comment

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello from master");
         System.out.println("hello world");
-        //kod niechciany
     }
 }
