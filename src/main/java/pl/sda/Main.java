48592648ecea26e41abd40e2a03c68bc94056efa
Piotr <p-czajkowski@outlook.com>
2018-01-13 14:24:44 +0100

    Krówka dla Dawida!

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello from master");
         System.out.println("hello world");
+
+        System.out.println("Krówka dla Dawida!!");
     }
 }
